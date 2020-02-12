fe7e5cc3779248568ca02d80640470f4bfe59ff7
Bader Ealyyah <bader.al.ealyyah@gmail.com>
2020-02-12 23:33:45 +0300

    texteditor

--- src/App.js
+++ src/App.js
@@ -12,15 +12,35 @@ const stylings = ["bold", "italic", "underline"];
 const colors = ["yellow", "blue", "red", "black", "purple"];
 
 class App extends Component {
+  state = {
+    textColor: "black",
+    bold: false,
+    italic: false,
+    underline: false
+  };
+
+  changeColor = newColor => {
+    this.setState({ textColor: newColor });
+  };
+  changeStyle = style => {
+    this.setState({ [style]: !this.state[style] });
+  };
+
   render() {
     const stylingBoxes = stylings.map(style => (
-      <button className="btn btn-light" style={styles[style]} key={style}>
+      <button
+        onClick={() => this.changeStyle(style)}
+        className="btn btn-light"
+        style={styles[style]}
+        key={style}
+      >
         {style}
       </button>
     ));
 
     const colorBoxes = colors.map(color => (
       <button
+        onClick={() => this.changeColor(color)}
         style={{ backgroundColor: color, height: 30, width: 30 }}
         key={color}
       />
@@ -29,7 +49,14 @@ class App extends Component {
     return (
       <div className="App">
         <div className="my-3">{stylingBoxes}</div>
-        <textarea />
+        <textarea
+          style={{
+            color: this.state.textColor,
+            fontWeight: this.state.bold ? "bold" : "",
+            fontStyle: this.state.italic ? "italic" : "",
+            textDecorationLine: this.state.underline ? "underline" : ""
+          }}
+        />
         <div className="my-3">{colorBoxes}</div>
       </div>
     );
